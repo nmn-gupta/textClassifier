06a24fb1f3db82d9f61ca4852f1b686d4dd0b555
Naman Gupta <48116263+nmn-gupta@users.noreply.github.com>
2020-08-07 17:49:51 +0530

    add templates in setting directory

--- textClassifier/textClassifier/settings.py
+++ textClassifier/textClassifier/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'textClassifier.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
