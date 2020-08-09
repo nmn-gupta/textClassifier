cf5bb399f90dfbbef0e42004a175655304be5f69
naman <naman.gupta_cs18@gla.ac.in>
2020-08-09 20:13:10 +0530

    add new type of partofspeech

--- textClassifier/textClassifier/views.py
+++ textClassifier/textClassifier/views.py
@@ -58,7 +58,9 @@ def translate(request):
             if i[1] == "VBD":
                 r += str(i[0]) + ": Verb, past tense!!" + "\n"
             if i[1] == "VBN":
-                r += str(i[0]) + ": Verb, past participle!!" + "\n"
+                r += str(i[0]) + ": Verb, past participle!" + "\n"
+            if i[1] == "PDT":
+                r += str(i[0]) + ": Predeterminer!" + "\n"
 
         params = {'purpose': 'Classified Text', 'converted_text': r}
         return render(request, 'analyze.html', params)
